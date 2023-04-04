b1110908fc87fa449235733e1d86ccd5b3bb7373
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-04-04 14:52:00 -0300

    :sparkles: feat: adicionando desenho de um quadrado

--- joglstart/src/main/java/com/mycompany/joglstart/JoglFrames.java
+++ joglstart/src/main/java/com/mycompany/joglstart/JoglFrames.java
@@ -26,13 +26,31 @@ public class JoglFrames implements GLEventListener {
     public void display(GLAutoDrawable drawable) {
         final GL2 gl = drawable.getGL().getGL2();
         gl.glBegin(GL2.GL_LINES);
-        gl.glVertex2d(0,0);
-        gl.glVertex2d(0,0.2);
+
+        // primeiro valor = posicao vertical
+        // segundo valor = posicao horizontal
+
+        // linha vertical esquerda
+        gl.glVertex2d(0, 0);
+        gl.glVertex2d(0, 0.4);
+
+        // linha vertical direita
+        gl.glVertex2d(0.4, 0);
+        gl.glVertex2d(0.4, 0.4);
+
+        // linha horizontal superior
+        gl.glVertex2d(0, 0.4);
+        gl.glVertex2d(0.4, 0.4);
+        
+        // linha horizontal inferior
+        gl.glVertex2d(0, 0);
+        gl.glVertex2d(0.4, 0);
+
         gl.glEnd();
     }
 
     @Override
     public void reshape(GLAutoDrawable glad, int i, int i1, int i2, int i3) {
     }
-    
+
 }
