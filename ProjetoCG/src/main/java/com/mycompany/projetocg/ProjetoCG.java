64bbebcf7b4fbf7ccf014964171efd916891178c
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-04-09 23:35:08 -0300

    :sparkles: feat: adicionando projeto da n1

---
+++ ProjetoCG/src/main/java/com/mycompany/projetocg/ProjetoCG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetocg;
+
+/**
+ *
+ * @author samuel
+ */
+public class ProjetoCG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
