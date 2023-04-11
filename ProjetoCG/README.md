e883fc1a241ec73abedf36eb721e186fd1b22940
thesamuelvitor <thesamuelvitor.py@gmail.com>
2023-04-10 23:39:49 -0300

    :sparkles: feat: adicionando funcao de mostragem de triangulo

---
+++ ProjetoCG/README.md
@@ -0,0 +1,9 @@
+# Projeto N1
+
+## Sobre a interoperabilidade
+
+A interoperabilidade entre o Java2D e o OpenGL é uma ferramenta poderosa que permite aos desenvolvedores aproveitar a potência do OpenGL em conjunto com a facilidade e familiaridade do Java2D para criar gráficos e animações sofisticados e eficientes.
+
+## Sobre Core OpenGL e o GLU
+
+Bibliotecas graficas que sao usadas para criação de gráficos 3D em aplicações de computador. Enquanto a Core OpenGL é responsável pela criação de baixo nível, a GLU é uma biblioteca de alto nível.
--- ProjetoCG/src/main/java/com/mycompany/projetocg/Cubo.java
+++ ProjetoCG/src/main/java/com/mycompany/projetocg/Cubo.java
@@ -1,85 +1,80 @@
 package com.mycompany.projetocg;
 
+import javax.swing.JFrame;
 import javax.media.opengl.GL2;
+import java.awt.event.WindowAdapter;
+import javax.media.opengl.GLProfile;
+
+import javax.media.opengl.awt.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import com.jogamp.newt.event.WindowEvent;
+
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 
 public class Cubo implements GLEventListener {
+  private GLCanvas canvas;
 
-  @Override
-  public void init(GLAutoDrawable glad) {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Penrose Triangle");
+    GLProfile glProfile = GLProfile.get(GLProfile.GL2);
+    GLCapabilities glCapabilities = new GLCapabilities(glProfile);
+    GLCanvas canvas = new GLCanvas(glCapabilities);
+    Cubo penrose = new Cubo();
+    canvas.addGLEventListener(penrose);
+    frame.add(canvas);
+    frame.setSize(400, 400);
+    frame.setVisible(true);
+    frame.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+    });
+  }
+
+  public void PenroseTriangle(GLCanvas canvas) {
+    this.canvas = canvas;
   }
 
   @Override
-  public void dispose(GLAutoDrawable glad) {
+  public void init(GLAutoDrawable drawable) {
+    GL2 gl = drawable.getGL().getGL2();
+    gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
   }
 
   @Override
   public void display(GLAutoDrawable drawable) {
+    GL2 gl = drawable.getGL().getGL2();
+    gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
 
-    final GL2 gl = drawable.getGL().getGL2();
-    gl.glBegin(GL2.GL_LINES);
-
-    // QUADRADO 1
-
-    // linha vertical esquerda
-    gl.glVertex2d(0, 0);
-    gl.glVertex2d(0, 0.4);
-
-    // linha vertical direita
-    gl.glVertex2d(0.4, 0);
-    gl.glVertex2d(0.4, 0.4);
-
-    // linha horizontal superior
-    gl.glVertex2d(0, 0.4);
-    gl.glVertex2d(0.4, 0.4);
-
-    // linha horizontal inferior
-    gl.glVertex2d(0, 0);
-    gl.glVertex2d(0.4, 0);
-
-    // QUADRADO 2
-
-    // linha vertical esquerda
-    gl.glVertex2d(0.2, 0.2);
-    gl.glVertex2d(0.2, 0.6);
-
-    // linha vertical direita
-    gl.glVertex2d(0.6, 0.2);
-    gl.glVertex2d(0.6, 0.6);
-
-    // linha horizontal superior
-    gl.glVertex2d(0.2, 0.6);
-    gl.glVertex2d(0.6, 0.6);
-
-    // linha horizontal inferior
-    gl.glVertex2d(0.2, 0.2);
-    gl.glVertex2d(0.6, 0.2);
-
-    // DIAGONAIS
-    
-    // linha vertical esquerda
-    gl.glVertex2d(0.4, 0.4);
-    gl.glVertex2d(0.6, 0.6);
-
-    // linha vertical direita
-    gl.glVertex2d(0.6, 0.2);
-    gl.glVertex2d(0.4, 0);
-
-    // linha horizontal superior
-    gl.glVertex2d(0.2, 0.2);
-    gl.glVertex2d(0, 0);
-
-    // linha horizontal inferior
-    gl.glVertex2d(0, 0.4);
-    gl.glVertex2d(0.2, 0.6);
-
+    // Desenha o primeiro triângulo
+    gl.glBegin(GL2.GL_TRIANGLES);
+    gl.glVertex2f(-0.7f, -0.7f);
+    gl.glVertex2f(0.7f, -0.7f);
+    gl.glVertex2f(0.0f, 0.7f);
     gl.glEnd();
 
+    // Desenha o segundo triângulo
+    gl.glColor3f(0.0f, 0.0f, 1.0f);
+    gl.glBegin(GL2.GL_TRIANGLES);
+    gl.glVertex2f(-0.7f, 0.7f);
+    gl.glVertex2f(0.7f, 0.7f);
+    gl.glVertex2f(0.0f, -0.7f);
+    gl.glEnd();
   }
 
   @Override
-  public void reshape(GLAutoDrawable glad, int i, int i1, int i2, int i3) {
+  public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    GL2 gl = drawable.getGL().getGL2();
+    gl.glViewport(0, 0, width, height);
+    gl.glMatrixMode(GL2.GL_PROJECTION);
+    gl.glLoadIdentity();
+    gl.glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    gl.glMatrixMode(GL2.GL_MODELVIEW);
+    gl.glLoadIdentity();
   }
 
+  @Override
+  public void dispose(GLAutoDrawable drawable) {
+  }
 }
